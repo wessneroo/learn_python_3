b70ccfbea72ed96ce6d99e816d84ad3591e00754
wessneroo <mwessner@me.com>
2025-11-15 22:15:47 -0700

    completed ascii block letters project

---
+++ initials.py
@@ -0,0 +1,20 @@
+# Matt Wessner
+# I am an engineer.
+
+ascii_M = """M   M 
+MM MM 
+MM MM 
+M M M 
+M   M 
+M   M 
+M   M \n\n""";
+
+ascii_W = """W   W 
+W   W 
+W   W 
+W W W 
+WW WW 
+WW WW 
+W   W \n""";
+
+print(ascii_M + ascii_W)
